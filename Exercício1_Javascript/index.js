9c971bdaaab36aeb6c9bce2abb1c4be30c19052f
Juliana Margon <jntmargon@gmail.com>
2022-04-11 15:57:23 -0300

    Inclusão do README

---
+++ Exercício1_Javascript/index.js
@@ -0,0 +1,10 @@
+let olderPersonName = prompt("Digite o nome da pessoa mais velha: ")
+let olderPersonAge = prompt("Digite a idade da pessoa mais velha: ")
+
+let youngerPersonName = prompt("Digite o nome da pessoa mais nova: ")
+let youngerPersonAge = prompt("Digite a idade da pessoa mais nova: ")
+
+let ageDiference = olderPersonAge - youngerPersonAge
+
+
+alert("A diferença entre as idades de " + olderPersonName + " e " + youngerPersonName + " é de " + ageDiference + " anos." )
